080cd3fe884373d3900e09db4b8b884870f7f4d0
Hassanhanzla <abulhassannokia@gmail.com>
2025-03-16 19:13:08 +0500

    strings
    python only

---
+++ strings.py
@@ -0,0 +1,12 @@
+for x in "Banana":
+    print(x)  #line wise letters
+    #lenght bof strings
+    a = "hellomy"
+    print(len(a))
+    #check the string
+    txt = "my name is abul hassan"
+    print("my" in txt) 
+    #if in str
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
